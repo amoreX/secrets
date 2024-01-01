00cab857219d42f96341f80f1fd81f6f5fefcd3d
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-01-01 22:03:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {dirname} from 'path';
+import { fileURLToPath } from 'url';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
+
+app.get('/', (req, res) => {
+    res.render('ind.ejs');
+    console.log("Website loaded succesfully");
+})
